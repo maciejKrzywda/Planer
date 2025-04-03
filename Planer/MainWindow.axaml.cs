1708bf6e7022df83730a026f9730361582fdafc1
Mk <maciej.krzywda@cyfroweszkoly.pl>
2025-04-03 20:44:12 +0200

    changed the location of class
    1.1

--- Planer/MainWindow.axaml.cs
+++ Planer/MainWindow.axaml.cs
@@ -8,6 +8,22 @@ using Avalonia.LogicalTree;
 using Avalonia.Media;
 
 namespace Planer;
+
+public class Task
+{
+    public String? Name;
+    public String Category;
+    public int CategoryIndex;
+    public Boolean IsFinished;
+        
+    public Task(String? taskName, String taskCategory, Boolean isFinished, int taskCategoryIndex)
+    {
+        Name = taskName;
+        Category = taskCategory;
+        IsFinished = isFinished;
+        CategoryIndex = taskCategoryIndex;
+    }
+}
 public partial class MainWindow : Window
 {
     public MainWindow()
